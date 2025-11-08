b5d247d6b84f7b48531f9ab81cb55c2be7ad1e71
noaarbely <noa.arbely@gmail.com>
2025-11-08 13:12:21 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,16 +1,12 @@
-def move(my_list, direction=None):
+def move(my_list, direction):
     index_of_one = my_list.index(1)
 
-    if direction == 'right':
-        # אם לא בקצה הימני
-        if index_of_one < len(my_list) - 1:
-            my_list[index_of_one] = 0
-            my_list[index_of_one + 1] = 1
+    if direction == 'right' and index_of_one < len(my_list) - 1:
+        my_list[index_of_one] = 0
+        my_list[index_of_one + 1] = 1
 
-    elif direction == 'left':
-        # אם לא בקצה השמאלי
-        if index_of_one > 0:
-            my_list[index_of_one] = 0
-            my_list[index_of_one - 1] = 1
+    elif direction == 'left' and index_of_one > 0:
+        my_list[index_of_one] = 0
+        my_list[index_of_one - 1] = 1
 
     return my_list
